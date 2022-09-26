1c5d1cc7016a547d7d21c420851b2b90daebd04c
maksimussokol <maksimus@2074.ru>
2022-09-27 00:40:52 +0500

    Ex3 doesn't work

---
+++ ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("enter the numbers");
+int[] array = new int[8];
+for (int i = 0; i < 9; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = array.Length;
+int position = 0;
+
+while (position < count)
+{
+    Console.WriteLine(array[position]);
+    position++;
+}
