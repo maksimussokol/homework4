d9d6ff1fb6f271e0bf3ac3ae07118e65f0e61d9c
maksimussokol <maksimus@2074.ru>
2022-09-27 00:22:32 +0500

    Ex2 works no krivo(took c chislami do 1000 znakov)

---
+++ ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("enter the number");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < 1000; i++)
+{
+    int f = n % 10;
+    n = n / 10;
+    sum = sum + f;
+}
+Console.WriteLine(sum);
