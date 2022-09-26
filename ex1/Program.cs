fed8df27c74931c6b6ff44d9523a6fad1dfdec07
maksimussokol <maksimus@2074.ru>
2022-09-27 00:04:27 +0500

    Ex1 works

---
+++ ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("enter A number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter B number");
+int b = Convert.ToInt32(Console.ReadLine());
+int f = Convert.ToInt32(Math.Pow(a, b));
+Console.WriteLine($"{a} v stepeni {b} = {f}");
+
